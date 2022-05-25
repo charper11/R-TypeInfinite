249896ec80e9f3f6c3674005c23b7dc0eb6fa372
Colby Harper <colby.harper11@gmail.com>
2022-05-24 22:53:36 -0500

    initial css and js code

---
+++ script.js
@@ -0,0 +1,42 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    // ctx = instance of built-in canvas 2D api that holds all drawing methods/properties
+    const ctx = canvas.getContext('2d');
+    canvas.width = 900;
+    canvas.height = 550;
+
+    //apply event listeners to keyboard events and hold array of all currently active keys
+    class InputHandler {
+
+    }
+
+    //react to keys as they are pressed, drawing/updating player
+    class Player {
+
+    }
+
+    //endlessly scrolling background
+    class Background {
+
+    }
+
+    //generate enemies
+    class Enemy {
+
+    }
+
+    //add, animate, and remove enemies
+    function handleEnemies(){
+
+    }
+
+    //display score and game over message
+    function displayStatusText(){
+
+    }
+
+    //main animation loop running at 60fps
+    function animate(){
+        
+    }
+});
